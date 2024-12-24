b41d650ae655f248d454b8f4989be56ff6b37d9c
Keith <34338831+keithrsialana@users.noreply.github.com>
2024-12-24 18:29:30 -0500

    created vite dev dependences, created working base react app

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<p>
+							&copy; {new Date().getFullYear()} Your Company Name. All rights
+							reserved.
+						</p>
+					</div>
+					<div className="col-md-6 text-right">
+						<ul className="list-inline">
+							<li>
+								<a href="#">Terms of Service</a>
+							</li>
+							<li>
+								<a href="#">Privacy Policy</a>
+							</li>
+							<li>
+								<a href="#">Contact Us</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
